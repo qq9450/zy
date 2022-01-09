9f0a2b1f1d3ab110de2c02e19b7ddcac511df8f2
qq9450 <86704264+qq9450@users.noreply.github.com>
2022-01-09 20:22:24 +0800

    Update sy.js

--- sy.js
+++ sy.js
@@ -2,7 +2,7 @@ var body = $response.body; // 声明一个变量body并以响应消息体赋值
 var obj = JSON.parse(body); // JSON.parse()将json形式的body转变成对象处理
 
 
-obj.vip={"isvip":1,"days":9999};
+obj.vip={"status":1,"days":9999};
 #obj.is_buy=12;
 #obj.signin_coun=0;
 
